c6959ddebc0cf91404695de5c6e4051d67e51b8a
Jimmy Lin <jimmydlin@gmail.com>
2019-11-10 19:46:02 -0800

    v4

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,30 +1,39 @@
 import React from "react"
 import { useCart } from "../redux/ducks/cart"
+import { useSelector } from 'react-redux'
+
+
 
 export default function (props) {
 const { cart, add, remove } = useCart()
-// const [ cart, setCart] =useState['']
+
 
 
     return (
-      
+      <div>
+          <i class="fa fa-shopping-cart"></i>
         <div id="checkout">
             {cart.map((cart, i) => (
                 <div id="indcheckout" key={'key' + i}>
-                <img src={`/assets/${cart.product.sku}_2.jpg`} />    
-                <div id="checkoutdesc">
-                    <p>{cart.product.title}</p>
-                    <p>{cart.product.description}</p>
-                    <button onClick={e => remove(cart.id)}>Remove from cart</button>
+                    <img src={`/assets/${cart.product.sku}_2.jpg`} />    
+                    <div id="checkoutdesc">
+                        <p>{cart.product.title}</p>
+                        <p>{cart.product.description}</p>
+                        <button onClick={e => remove(cart.id)}>Remove from cart</button>
+                    </div>
+                    <div>${cart.product.price}</div>
+                
                 </div>
-                <p>${cart.product.price}</p>
-              
-                </div>
+                
             ))}
-            
+            <p id="checkoutbutton">CHECKOUT</p>
         </div>
-        
        
+     </div>
+      
     )
+
+
     }
 
+ 
--- src/components/main.js
+++ src/components/main.js
@@ -7,22 +7,33 @@ const { products, add, remove } = useCart()
  
 
     return (
+       <div id="center">
        
+        <div id="maintop">
+                <div>{products.length} product(s) found</div>
+                <div id="maintopright">
+                <span>Order by</span>
+                <select>
+                    <option>Select</option>
+                    <option>Highest to lowest</option>
+                    <option>Lowest to highest</option>
+                </select>
+                </div>
+        </div>
         <div id="main">
-            
             {products.map((product, i) => (
                 <div id="individual" key={'key' + i}>
-                <img src={`/assets/${product.sku}_1.jpg`} />    
-                <p>{product.title}</p>
-                <i class="fa fa-horizontal-rule"></i>
-                <p>${product.price}</p>
-                <p>or {product.installments} X${(product.price / product.installments).toFixed(2)}</p>
-                <button onClick={e => add(product)}>Add to cart</button>
+                    <img src={`/assets/${product.sku}_1.jpg`} />    
+                    <p>{product.title}</p>
+                    <i class="fa fa-horizontal-rule"></i>
+                    <p>${product.price}</p>
+                    <p>or {product.installments} X${(product.price / product.installments).toFixed(2)}</p>
+                    <button onClick={e => add(product)}>Add to cart</button>
                 </div>
             ))}
             
         </div>
-      
+      </div>
        
     )
     }
